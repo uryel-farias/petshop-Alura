fac98ab6e6c65786b6cc3857132544d73ed4bc34
Uryel Farias <uryel.farias@gmail.com>
2023-04-28 07:07:03 -0300

    Validação do campo CPF concluida

--- vadacao.js
+++ vadacao.js
@@ -42,11 +42,20 @@ const mensagensDeErro = {
     dataNascimento: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio.',
         customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+    },
+    cpf: {
+        valueMissing: 'O campo de CPF não pode estar vazio.',
+         customError: 'O CPF digitado não é válido.'
+    },
+    cep: {
+        valueMissing: 'O campo de CEP não pode estar vazio.',
+        patternMismatch: 'O CEP digitado não é valido'
     }
 }
 
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento:input => validaDataNascimento(input),
+    cpf:input => validaCPF(input)
 
 }
 
@@ -77,4 +86,75 @@ function maiorQue18(data){
     const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
 
     return dataMais18 <= dataAtual;
+}
+
+
+function validaCPF(input){
+    const cpfFormatado = input.value.replace(/\D/g, '');
+    let mensagem = '';
+
+    if(!checaCpfRepetido(cpfFormatado) || !checaEstruturaCpf(cpfFormatado)){
+        mensagem = 'O CPF digitado não é válido.'
+    }
+
+    input.setCustomValidity(mensagem);
+}
+
+function checaCpfRepetido (cpf){
+    const valoresRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ];
+
+
+    let cpfValido = true;
+
+    valoresRepetidos.forEach(valor=>{
+        if(valor == cpf){
+            cpfValido = false;
+        }
+    })
+
+    return cpfValido;
+}
+
+
+function checaEstruturaCpf(cpf) {
+    const multiplicador = 10;
+    
+    return checaDigitoVerificador(cpf, multiplicador);
+}
+
+function checaDigitoVerificador(cpf, multiplicador){
+    if(multiplicador >= 12){
+        return true;
+    }
+    
+    let soma = 0;
+    let multiplicadorInicial = multiplicador;
+    const cpfSemDigitos = cpf.substr(0,multiplicador -1).split('');
+    const digitoVerificador = cpf.charAt(multiplicador - 1);
+
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial;
+        contador++;
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1)
+    }
+
+    return false;
+}
+
+function confirmaDigito(soma){
+    return 11 - (soma % 11);
 }
